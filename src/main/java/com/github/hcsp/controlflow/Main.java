2ce8c730b15a9c7b8f5f9f972397ab0ecc2ddcc7
zhangyongwang <20225614@qq.com>
2019-06-13 11:31:07 +0800

    for循环语句，打印区间内的奇数。

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,16 +12,11 @@ public class Main {
      */
     public static void printOddNumbersBetween(int start, int end) {
         for (int i = start; i <= end; i++) {
-                if (i % 2 == 0) {
-                    continue;
-                }
-                if (i == end) {
-                    System.out.println(i);
-                    continue;
-                }
+            if (i % 2 != 0) {
                 System.out.print(i + ",");
             }
         }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
