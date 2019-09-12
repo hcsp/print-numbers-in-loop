29d61507d99150d75b6740698da5c7ae87cf418e
steven <951520698@qq.com>
2019-09-12 16:33:33 +0800

    居然没有注意到逗号，哎。。。

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -27,6 +27,6 @@ public class Main {
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
-        printOddNumbersBetween(-75, 75);
+        printOddNumbersBetween(-2, 2);
     }
 }
