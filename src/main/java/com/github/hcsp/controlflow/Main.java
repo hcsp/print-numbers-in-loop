d19213dd76de8753e91d7d246cabd74c713379c5
Fang Lu <huzhengen@gmail.com>
2019-12-23 15:23:54 +0800

    print numbers in loop (#115)
    print numbers in loop (#115)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,15 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        StringBuilder str = new StringBuilder();
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                str.append(i).append(",");
+            }
+        }
+        System.out.println(str.substring(0, str.length() - 1));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
