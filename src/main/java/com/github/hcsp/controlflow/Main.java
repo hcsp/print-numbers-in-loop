60d25cbebaf083cda41139c47b999870bd094b11
陶思亮 <32303633+tsl1127@users.noreply.github.com>
2019-12-26 21:36:40 +0800

    数字区间找出奇数 (#118)
    数字区间找出奇数 (#118)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -7,9 +7,18 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        //StringBuffer 和 StringBuilder 类的对象能够被多次的修改，并且不产生新的未使用对象
+        StringBuilder str = new StringBuilder(); //StringBuilder比StringBuffer有速度优势,StringBuffer比StringBuilder有线程安全
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                str.append(i).append(',');
+            }
+        }
+        System.out.println(str.substring(0, str.length() - 1));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
