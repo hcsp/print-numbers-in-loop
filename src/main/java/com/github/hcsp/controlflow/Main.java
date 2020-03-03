fee5e9402b148056738b804d06e33e02e68f5051
Truthcode <617805327@qq.com>
2020-03-03 17:00:50 +0800

    aaa

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -9,7 +9,16 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        while (start <= end){
+            if (start % 2 != 0){
+                System.out.print(start + ",");
+            }
+            start++;
+        }
+        System.out.println();
+    }
+
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
