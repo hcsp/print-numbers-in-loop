7d75d24983bc77bf9ae92bd631e32abec22bc4c7
zhuxueqiao <60607893+zhuxueqiao@users.noreply.github.com>
2020-03-08 01:38:01 +0800

    输出质数 (#161)
    输出质数 (#161)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.controlflow;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     /**
      * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
@@ -7,12 +13,26 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i);
+                if (start <= end - 1) {
+                    System.out.print(",");
+                }
+            }
+        }
+
+
+
+    }
 
     public static void main(String[] args) {
-        printOddNumbersBetween(1, 5);
+
+         printOddNumbersBetween(1, 5);
         printOddNumbersBetween(-2, 2);
     }
 }
