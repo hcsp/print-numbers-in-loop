3a40e124fa9f5a627c8669ce25fb65838e9d6329
zhangzh <zhangzh@cnlemon.net>
2020-04-18 12:14:55 +0800

    feat: 完成挑战

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,10 @@
 package com.github.hcsp.controlflow;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     /**
      * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
@@ -7,9 +12,18 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        List<String> result = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            result.add(Integer.toString(i));
+        }
+        System.out.println(String.join(",", result));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
