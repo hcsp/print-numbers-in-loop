802f7dc66d17afc38333b0e7420c5f36975f9e64
Administrator <756433556@qq.com>
2020-05-15 23:17:47 +0800

    循环打印指定的数字 修正

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void printOddNumbersBetween(int start, int end) {
         for (int i = start; i <= end; i++) {
             if (i % 2 != 0) {
-                System.out.println(i == end ? i + "\n" : i + ",,");
+                System.out.println(i == end ? i + "\n" : i + ",");
             }
         }
     }
