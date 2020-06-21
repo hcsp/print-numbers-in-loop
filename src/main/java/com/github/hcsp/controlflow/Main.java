9369bcb555f14b15a0d1c9989255e66f27925ca2
陳雲濤 <36143626+0x522@users.noreply.github.com>
2020-06-22 00:04:20 +0800

    写出区间内的奇数。 (#209)
    写出区间内的奇数。 (#209)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     /**
      * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
@@ -7,9 +10,21 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        if (start % 2 == 0) {
+            start += 1;
+        }
+        while (start <= end) {
+            if (start == end || start == end - 1) {
+                System.out.print(start);
+            }else {
+                System.out.print(start+",");
+            }
+                start += 2;
+        }
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
