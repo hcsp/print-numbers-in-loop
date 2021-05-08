e8012a3252a891d3e5cad2913405dba63de97b9b
HornsWong <fanciwong@protonmail.com>
2021-05-08 16:18:27 +0800

    已修正 (#244)
    已修正 (#244)

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.controlflow;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     /**
      * 打印从start到end区间所有的奇数，包括start和end本身（若符合条件）。 注意，数字之间用英文逗号分隔。
@@ -9,7 +12,15 @@ public class Main {
      * @param start 区间开始
      * @param end 区间结束
      */
-    public static void printOddNumbersBetween(int start, int end) {}
+    public static void printOddNumbersBetween(int start, int end) {
+        List<String> list = new ArrayList<>();
+        for (int i = start; i < end + 1; i++) {
+            if (i % 2 != 0) {
+                list.add(String.valueOf(i));
+            }
+        }
+        System.out.println(String.join(",", list));
+    }
 
     public static void main(String[] args) {
         printOddNumbersBetween(1, 5);
