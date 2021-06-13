bea5d04a0021f1e096e637b502a62dcd701c99e5
jingxin wang <wjxalexander@hotmail.com>
2021-06-14 01:29:01 +0800

    done

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,12 +10,12 @@ public class Main {
      * <p>例如，start=1,end=5，则打印1,3,5 又如，start=-2,end=2，则打印-1,1
      *
      * @param start 区间开始
-     * @param end 区间结束
+     * @param end   区间结束
      */
     public static void printOddNumbersBetween(int start, int end) {
-        ArrayList<Integer> list =new ArrayList<>();
-        for(int i = start ; i < end + 1; i++){
-            if(i % 2 != 0){
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = start; i < end + 1; i++) {
+            if (i % 2 != 0) {
                 list.add(i);
             }
         }
