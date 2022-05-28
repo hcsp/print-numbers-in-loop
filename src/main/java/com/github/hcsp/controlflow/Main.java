64fdf5deba96782a2e3704aad80eed588b5e46c7
3stonelei <1462070965@qq.com>
2022-05-28 11:03:36 +0800

    奇数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -10,11 +10,14 @@ public class Main {
      * @param end 区间结束
      */
     public static void printOddNumbersBetween(int start, int end) {
+        String s="";
         for (int i = start; i <=end ; i++) {
             if (i%2!=0){
-                System.out.println(i);
+
+                s+=i+",";
             }
         }
+        System.out.println(s.toString());
     }
 
     public static void main(String[] args) {
